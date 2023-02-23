58cb1ccafee13cecb32e2c0a06200fbcb9470d1f
Aman Murya <m.amanmaurya49@gmail.com>
2023-02-23 09:36:37 +0530

    added logo at navbar

--- index/script.js
+++ index/script.js
@@ -1,40 +1,43 @@
 function validateForm() {
-    const startDate = document.querySelector("#startDate").value;
-    const startTime = document.querySelector("#startTime").value;
-    const endDate = document.querySelector("#endDate").value;
-    const endTime = document.querySelector("#endTime").value;
+  const startDate = document.querySelector('#startDate').value;
+  const startTime = document.querySelector('#startTime').value;
+  const endDate = document.querySelector('#endDate').value;
+  const endTime = document.querySelector('#endTime').value;
 
-    if (startDate === "" || startTime === "" || endDate === "" || endTime === "") {
-      alert("All fields are required");
-      return false;
-    }
-
-    if (startDate > endDate) {
-      alert("Start date should be before end date");
-      return false;
-    }
-
-    if (startDate === endDate && startTime >= endTime) {
-      alert("Start time should be before end time");
-      return false;
-    }
-
-    return true;
+  if (
+    startDate === '' ||
+    startTime === '' ||
+    endDate === '' ||
+    endTime === ''
+  ) {
+    alert('All fields are required');
+    return false;
   }
 
+  if (startDate > endDate) {
+    alert('Start date should be before end date');
+    return false;
+  }
 
+  if (startDate === endDate && startTime >= endTime) {
+    alert('Start time should be before end time');
+    return false;
+  }
 
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-const links = document.querySelectorAll(".nav-links li");
+  return true;
+}
 
-hamburger.addEventListener('click', ()=>{
-   //Animate Links
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    });
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
 
-    //Hamburger Animation
-    hamburger.classList.toggle("toggle");
+hamburger.addEventListener('click', () => {
+  //Animate Links
+  navLinks.classList.toggle('open');
+  links.forEach((link) => {
+    link.classList.toggle('fade');
+  });
+
+  //Hamburger Animation
+  hamburger.classList.toggle('toggle');
 });
